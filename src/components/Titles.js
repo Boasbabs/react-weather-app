2e6ecb23657fd0c7263515b147771d3a62faf8a5
Boasbabs <babalolasimeon@gmail.com>
2018-05-08 13:41:36 +0000

    Add CSS styles and image

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -17,8 +17,8 @@ class Titles extends React.Component {
 // Using stateless function
 const Titles = (props) => (
     <div>
-        <h1>Weather Finder</h1>
-        <p>Find out temperature, conditions and more...</p>
+        <h1 className="title-container__title">Weather Finder</h1>
+        <p className="title-container__subtitle">Find out temperature, conditions and more...</p>
     </div>
 )
 
